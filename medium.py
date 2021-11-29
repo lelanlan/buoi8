8778b70438caf3c35dd8bda5651b74c0f3a3bb7f
lelanlan <90110488+lelanlan@users.noreply.github.com>
2021-11-29 19:58:07 +0700

    Update medium.py

--- medium.py
+++ medium.py
@@ -10,11 +10,11 @@ def anagram_number(num):
         return False
 
 # Đổi số la mã
-def from_roman(num):
+def roman_to_int(num):
     roman_numerals = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
     result = 0
     for i,c in enumerate(num):
-        if (i+1) == len(num) or roman_numerals[c] > roman_numerals[num[i+1]]:
+        if (i+1) == len(num) or roman_numerals[c] >= roman_numerals[num[i+1]]:
             result += roman_numerals[c]
         else:
             result -= roman_numerals[c]
